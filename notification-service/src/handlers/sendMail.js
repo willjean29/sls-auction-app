e73dc8e733a6fe2d2f9b3f8dbc93e0e6ae655ca1
Jean Osco <willjean29@gmail.com>
2023-10-31 16:47:04 -0500

    feat(notification): implement send mail function with ses

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,6 +1,30 @@
+import AWS from 'aws-sdk';
+const ses = new AWS.SES({ region: 'us-east-1' });
+
 async function sendMail(event, context) {
-  console.log({ event })
-  return event;
+  const paraams = {
+    Source: 'willjean29@gmail.com',
+    Destination: {
+      ToAddresses: ['willjean29@gmail.com']
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from AWS SES'
+        }
+      },
+      Subject: {
+        Data: 'Test email from AWS SES'
+
+      }
+    }
+  }
+  try {
+    const result = await ses.sendEmail(paraams).promise();
+    console.log({ result })
+  } catch (error) {
+    console.log({ error });
+  }
 }
 
 export const handler = sendMail;
