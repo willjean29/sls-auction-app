d9b1b473aafa2ef0fbf17ca5739ea43e8780b35a
Jean Osco <willjean29@gmail.com>
2023-11-07 16:02:57 -0500

    feat(auction): save pictureurl in dyanmodb

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -3,24 +3,25 @@ import { InternalServerError } from 'http-errors';
 import httpErrorHandler from '@middy/http-error-handler';
 import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
 import { getAuctionById } from "./getAuction";
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
   const auction = await getAuctionById(id);
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
-
+  let updateAuction;
   try {
-    const uploadToS3Result = await uploadPictureToS3(auction.id + ".jpg", buffer);
-    console.log({ uploadToS3Result })
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updateAuction = await setAuctionPictureUrl(id, pictureUrl);
   } catch (error) {
     console.log(error);
-    throw new InternalServerError(error)
+    throw new InternalServerError(error);
   }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updateAuction),
   }
 }
 
