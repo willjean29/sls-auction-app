b66e110a62ae7cadb594739eb55d7a561f198cd3
Jean Osco <willjean29@gmail.com>
2023-11-07 14:09:01 -0500

    feat(auction): upload pictures to aws s3

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,4 +1,14 @@
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+import { getAuctionById } from "./getAuction";
+
 export async function uploadAuctionPicture(event) {
+  const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64, "base64");
+  console.log({ body: event.body, buffer });
+  const uploadToS3Result = await uploadPictureToS3(auction.id + ".jpg", buffer);
+  console.log({ uploadToS3Result })
   return {
     statusCode: 200,
     body: JSON.stringify({}),
